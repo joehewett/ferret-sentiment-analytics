00a2acadf170b82639583f4b6227c2ce6e32fe2d
Joe Hewett <joehewett1@gmail.com>
2021-01-15 11:39:06 +0000

    Add auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
