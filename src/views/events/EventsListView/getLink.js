a6867d4654ea7002674c5787ba57900aae901de1
khaokong <khaokong@gmail.com>
2021-03-03 19:47:24 +0700

    Add QRCode/clipboard copy for Feedback Form

--- src/views/events/EventsListView/getLink.js
+++ src/views/events/EventsListView/getLink.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+// npm install --save react-qr-code
+import QRCode from 'react-qr-code';
+// npm install --save use-clipboard-copy
+import { useClipboard } from 'use-clipboard-copy';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import LinkIcon from '@material-ui/icons/Link';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
-import { API } from 'aws-amplify';
+import TextField from '@material-ui/core/TextField';
 import { grey } from '@material-ui/core/colors';
-import { createEvent as createEventMutation } from '../../../graphql/mutations';
 /* eslint react/prop-types: 0 */
 
-const initialFormState = { name: '', description: '' };
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
@@ -29,10 +30,6 @@ const useStyles = makeStyles((theme) => ({
   descDesign: {
     background: 'white',
   },
-  typology: {
-    fontSize: 24,
-    color: 'red',
-  },
   button: {
     fontSize: '1rem',
     size: 'small',
@@ -41,45 +38,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function GetLinkButton({
-  events,
-  setEvents,
-  eventCount,
-  setEventCount,
   eventid,
 }) {
+  const clipboard = useClipboard();
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
-  const [formData, setFormData] = useState(initialFormState);
   const handleClickOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
   };
-  async function createEvent() {
-    if (!formData.name && !formData.description) return;
-    console.log('createEventMutation');
-    console.log(formData.name);
-    console.log(formData.description);
-    try {
-      await API.graphql({
-        query: createEventMutation,
-        variables: {
-          input: formData
-        },
-        authMode: 'AMAZON_COGNITO_USER_POOLS'
-      });
-      setOpen(false);
-      console.log('createEventMutation success');
-      console.log('data form is: ', formData);
-      setEvents([...events, formData]);
-      console.log('EventCreated');
-      setEventCount(eventCount + 1);
-      console.log('yes');
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
+  const eventURL = `/app/feedback/${eventid}`;
+
+  const handleCopyLink = () => {
+    clipboard.copy(eventURL); // programmatically copying a value
+  };
 
   return (
     <div>
@@ -95,47 +70,40 @@ export default function GetLinkButton({
         Get Link
       </Button>
       <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
-        <DialogTitle className={classes.typology} size="small">Get Event Link</DialogTitle>
+        <DialogTitle className={classes.typology} color="primary" size="large">Get Event Link</DialogTitle>
         <DialogContent>
           <Grid
+            container
+            size="small"
+            display="inline"
+          >
+            <TextField
+              style={{
+                fontSize: '3px',
+                width: '100%',
+                height: '100%'
+              }}
+              size="small"
+              variant="outlined"
+              ref={clipboard.target}
+              value={eventURL}
+              readOnly
+            />
+          </Grid>
+          <QRCode value={eventURL} />
+          <br />
+          <Button
+            startIcon={<FileCopyIcon />}
+            onClick={handleCopyLink}
+            color="primary"
             size="small"
           >
-            <p>
-              /app/feedback/
-              {eventid}
-              <Button startIcon={<FileCopyIcon />} />
-            </p>
-          </Grid>
-          <form className={classes.root}>
-            <div>
-              <TextField
-                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                value={formData.name}
-                id="outlined-basic"
-                label="Event Name"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-                value={formData.description}
-                id="outlined-multiline-static"
-                label="Event Description"
-                multiline
-                rows={4}
-                variant="outlined"
-              />
-            </div>
-          </form>
+            Copy Feedback Link
+          </Button>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            <LinkIcon />
-            Cancel
-          </Button>
-          <Button onClick={createEvent} color="primary">
-            Ok
+            Close
           </Button>
         </DialogActions>
       </Dialog>
